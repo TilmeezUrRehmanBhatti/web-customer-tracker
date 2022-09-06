529526f12f4ad963ffecf4838dbe8a12c2b57e4d
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-06 14:48:12 +0200

    List Customers - Developing Hibernate DAO

---
+++ src/main/java/com/tilmeez/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.tilmeez.springdemo.dao;
+
+import com.tilmeez.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    public List<Customer> getCustomers();
+}
