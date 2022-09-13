1e253b843eaa4e4b99ea0c1407bc49d941648aea
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-13 13:49:33 +0200

    Setup poincut declarations

--- src/main/java/com/tilmeez/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/tilmeez/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.tilmeez.springdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -13,6 +14,27 @@ public class CRMLoggingAspect {
     private Logger myLogger = Logger.getLogger(getClass().getName());
 
     //setup pointcut declarations
+    @Pointcut("execution(* com.tilmeez.springdemo.controller.*.*(..))")
+    private void forControllerPackage() {
+
+    }
+
+    // do the same for server and dao
+    @Pointcut("execution(* com.tilmeez.springdemo.service.*.*(..))")
+    private void forServicePackage() {
+
+    }
+
+    @Pointcut("execution(* com.tilmeez.springdemo.dao.*.*(..))")
+    private void forDaoPackage() {
+
+    }
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow() {
+
+    }
+
     //Add @Before advice
     //Add @AfterReturning advice
 }
