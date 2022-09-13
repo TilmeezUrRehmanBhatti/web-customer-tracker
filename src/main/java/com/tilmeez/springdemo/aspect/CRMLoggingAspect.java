a274e1a92a5e72c454310bb205d8c9dafbdb75ce
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-13 14:17:36 +0200

    Add @AfterReturning advice

--- src/main/java/com/tilmeez/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/com/tilmeez/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,9 @@
 package com.tilmeez.springdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -36,7 +39,42 @@ public class CRMLoggingAspect {
     }
 
     //Add @Before advice
+    @Before("forAppFlow()")
+    public void before(JoinPoint theJoinPoint) {
+
+        // display method we are calling
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("======>>> in @Before: calling method: " + theMethod);
+
+        // display the argument tp the method
+
+        // get the arguments
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop thru and display args
+        for (Object tempArg : args) {
+            myLogger.info("=====>> argument: " + tempArg);
+        }
+
+
+    }
     //Add @AfterReturning advice
+
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult"
+    )
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+
+        // display method we are returning from
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("======>>> in @AfterReturning: from method: " + theMethod);
+
+        // display data return
+        myLogger.info("====>>> result: " + theResult);
+
+
+    }
 }
 
 
